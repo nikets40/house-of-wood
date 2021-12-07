e3f76bea04c5c579777e05ac20fd6d50870afae2
Niket <40358714+nikets40@users.noreply.github.com>
2021-12-07 09:19:27 +0530

    admin dashboard switched to route based switching

--- components/admin/Sidebar.tsx
+++ components/admin/Sidebar.tsx
@@ -1,55 +1,76 @@
 import { ShoppingBag, Plus, Settings, LogOut } from "react-feather";
 import Image from "next/dist/client/image";
-import { MouseEventHandler, useState } from "react";
+import React from "react";
+import Link from "next/dist/client/link";
+import { useRouter } from "next/router";
+import ManageProducts from "./ManageProducts";
+import AddProduct from "./AddProduct";
+import AdminSettings from "./AdminSettings";
 
-const Sidebar: React.FC<{
-  onPageChange?: Function;
-  onLogout?: MouseEventHandler;
-}> = ({ onPageChange, onLogout }) => {
-  const divider = <div className="w-full h-px bg-primary mt-3 mb-2" />;
-
-  const [selectedIndex, setSelectedIndex] = useState(0);
+const tabsData = [
+  {
+    slug: "manage-products",
+    label: "Manage Products",
+    Icon: <ShoppingBag />,
+    Component: <ManageProducts />,
+  },
+  {
+    slug: "add-product",
+    label: "Add Product",
+    Icon: <Plus />,
+    Component: <AddProduct />,
+  },
+  {
+    slug: "settings",
+    label: "Admin Settings",
+    Icon: <Settings />,
+    Component: <AdminSettings />,
+  },
+];
+const Sidebar: React.FC<{ onTabChange?: Function }> = ({ onTabChange }) => {
+  const router = useRouter();
+  const currentPath = router.query.tab;
 
   return (
     <aside className="w-min shadow-lg px-4 flex flex-col gap-4 text-base h-screen pt-4">
-      <Image
-        src="/favicon.png"
-        width="45px"
-        height="45px"
-        objectFit="contain"
-      />
-      {divider}
-      <SidebarItem
-        onClick={() => {
-          setSelectedIndex(0);
-          onPageChange(0);
-        }}
-        isSelected={selectedIndex === 0}
-        Icon={<ShoppingBag />}
-        title="Manage Products"
-      />
-      <SidebarItem
-        onClick={() => {
-          setSelectedIndex(1);
-          onPageChange(1);
-        }}
-        isSelected={selectedIndex === 1}
-        Icon={<Plus />}
-        title="Add Product"
-      />
-      {divider}
-      <SidebarItem
-        onClick={() => {
-          setSelectedIndex(2);
-          onPageChange(2);
-        }}
-        isSelected={selectedIndex === 2}
-        Icon={<Settings />}
-        title="Settings"
-      />
+      <Link href="/admin" passHref>
+        <Image
+          src="/favicon.png"
+          width="45px"
+          height="45px"
+          objectFit="contain"
+          className="cursor-pointer"
+          onClick={() => onTabChange()}
+        />
+      </Link>
+
+      <SidebarDivider />
+
+      {tabsData.map(({ Icon, label, slug, Component }, index) => {
+        return (
+          <React.Fragment key={index}>
+            <SidebarItem
+              Icon={Icon}
+              title={label}
+              slug={slug}
+              key={index + slug}
+              isSelected={currentPath === slug}
+              onClick={() => onTabChange(Component)}
+            />
+            {index == 1 && <SidebarDivider />}
+          </React.Fragment>
+        );
+      })}
 
       <div className="flex-grow" />
-      <SidebarItem onClick={onLogout} Icon={<LogOut />} title="Log out" />
+      <SidebarItem
+        onClick={() => {
+          localStorage.setItem("isAdmin", "false");
+          router.reload();
+        }}
+        Icon={<LogOut />}
+        title="Log out"
+      />
       <div className="h-2" />
     </aside>
   );
@@ -57,14 +78,37 @@ const Sidebar: React.FC<{
 
 export default Sidebar;
 
-const SidebarItem: React.FC<{
+interface SidebarItemProps {
   Icon: React.ReactNode;
   title: string;
-  onClick?: MouseEventHandler<HTMLElement>;
+  onClick?: Function;
   isSelected?: boolean;
-}> = ({ Icon, title, onClick, isSelected }) => {
+  slug?: string;
+}
+
+const SidebarItem: React.FC<SidebarItemProps> = ({
+  Icon,
+  title,
+  onClick,
+  slug,
+  isSelected = false,
+}) => {
+  const router = useRouter();
   return (
-    <div onClick={onClick} className="group relative flex cursor-pointer">
+    <div
+      onClick={() => {
+        onClick && onClick();
+        if (slug) {
+          // router.push(`/admin/${slug}`);
+          router.push({
+            query: {
+              tab: slug,
+            },
+          });
+        }
+      }}
+      className="group relative flex cursor-pointer"
+    >
       <div className={`sidebar-icon ${isSelected && "sidebar-icon-selected"}`}>
         {Icon}
       </div>
@@ -72,3 +116,7 @@ const SidebarItem: React.FC<{
     </div>
   );
 };
+
+const SidebarDivider: React.FC = () => (
+  <div className="w-full h-px bg-primary mt-3 mb-2" />
+);
