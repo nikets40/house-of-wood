c0726cd01ef6189a81ab6b6eca2f68005e8e8162
Niket <40358714+nikets40@users.noreply.github.com>
2021-12-08 10:19:53 +0530

    build issue resolved and eslint added

--- lib/context.ts
+++ lib/context.ts
@@ -1,4 +1,9 @@
 import { createContext } from "react";
-import UserContextInterface from "../interfaces/usercontextInterface";
+
+import { User } from "firebase/auth";
+interface UserContextInterface {
+    user: User | null;
+    username: string;
+}
 
 export const UserContext = createContext<UserContextInterface | null>(null);
